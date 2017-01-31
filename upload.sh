3511d057c2632cd40dbc3efaccd30d14d868ed49
Tim Abrahamsen <timmy_neutron@mac.com>
2017-01-31 07:42:09 -0500

    cleaning directory

--- upload.sh
+++
@@ -1,9 +0,0 @@
-rsync -avz ~/Desktop/steeringangle8/IMG/ carnd@54.165.133.205:~/steeringangle8/IMG
-
-scp ~/Desktop/steeringangle8/driving_log.csv carnd@54.165.133.205:~/steeringangle8/driving_log.csv
-
-scp ~/Desktop/steeringangle8/timmynet.py carnd@54.165.133.205:~/steeringangle8/timmynet.py
-
-scp ~/Desktop/steeringangle8/preprocess.py carnd@54.165.133.205:~/steeringangle8/preprocess.py
-
-scp ~/Desktop/steeringangle8/load.py carnd@54.165.133.205:~/steeringangle8/load.py
